88714a450eb7dbd9a5731cde8ad1d75a5e3902bb
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-12 16:49:52 +0500

    Добавьте файлы проекта.

---
+++ UsingSwitchAndRandom/UsingSwitchAndRandom.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int num;
+    srand(2);
+    for (int k = 1;k <= 10;k++) {
+        //Случайное число от 2 до 8:
+        num = 2 + rand() % 7;
+        switch (num) {
+        case 3:
+        case 6:
+            cout << num << ": число делится на три" << endl;
+            break;
+        case 2:
+        case 4:
+        case 8:
+            cout << num << ": число является степенью двух" << endl;
+            break;
+        case 5:
+            cout << num << ": пятёрка" << endl;
+            break;
+        case 7:
+            cout << num << ": семёрка" << endl;
+            break;
+        }
+    }
+    system("pause > nul");
+    return 0;
+}
